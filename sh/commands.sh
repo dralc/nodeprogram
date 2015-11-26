327c043b27302117ce00bcb0a85ea5b45dfd1f2b
Aloysius Chan <aloysius.chan@news.com.au>
2015-11-26 15:07:05 +1100

    - working with commands info

--- sh/commands.sh
+++ sh/commands.sh
@@ -14,6 +14,7 @@ cd ~user_name # change to user_name's home dir
 
 ls dir1 dir2  # list contents of 1+ dirs
 ls -A         # doesn't list the . and ..
+ls -i         # Print the inode no. in the first field. inode no. is used to verify if any 2 files/hardlinks are the same
 
 file file_name   # file type info
 # Everything in Unix is a file
@@ -28,4 +29,13 @@ cd /usr   # user programs and their support files
 cp *.html DEST           # batch copy files
 cp --recursive dir1 DEST # copy dir1 AND it's contents
 mkdir dir1 dir2...       # make multiple dirs
+
+rm *.html   # tip: Check the wildcard with ls first before using rm !
+
+ln -s origFile link  # symbolic link
+
+type command_name   # Info about the type (binary, script, alias) of the command
+which command_name  # Gives the location of the command
+help command_name   # help for built-in commands
+apropos search | man -k search   # Propose a list of commands matching search
 
