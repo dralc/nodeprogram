1fb3f71f5eccad9aef1ad5beaec3ee3dc5dee446
Chan, Aloysius <aloysius.chan@news.com.au>
2016-07-06 16:49:09 +1000

    - git list changed files between commits

--- git/git.sh
+++ git/git.sh
@@ -10,3 +10,8 @@ git config -l
 # Clone a specific branch only
 git clone URL --branch branchName --single-branch myFolder
 
+# List all files changed between two commits
+# - http://stackoverflow.com/questions/3144647/git-show-all-files-changed-between-two-commits
+git log --name-only --pretty=oneline --full-index SHA-1A..SHA-1B |grep --invert-match --extended-regexp '^[0-9a-f]{40} ' |sort |uniq
+
+git diff --stat SHA-1A SHA-1B
